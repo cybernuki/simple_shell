ae38bcaae11675d3ea385221efb23a283e133b07
jhonatan arenas <1164@holbertonschool.com>
2019-11-27 23:29:04 +0000

    exit built-in

--- _getline.c
+++ _getline.c
@@ -3,10 +3,11 @@
 ssize_t _getline(char **buf)
 {
     ssize_t result = 0;
-    size_t var = 0; 
+    size_t var = 0;
 
     result = getline(buf, &var,stdin);
-    
 
-    return (result);  
+    if (_strcmp(*buf , EXIT_CMD) == 0)
+	    result = -1;
+    return (result);
 }
