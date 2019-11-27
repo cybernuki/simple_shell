063b76fd9d63890a8bc34e142f4bd55257eb4037
Feli <srgatoiscool@gmail.com>
2019-11-27 23:34:59 +0000

    added exit

--- _getline.c
+++ _getline.c
@@ -8,6 +8,9 @@ ssize_t _getline(char **buf)
     result = getline(buf, &var,stdin);
 
     if (_strcmp(*buf , EXIT_CMD) == 0)
-	    result = -1;
+    {
+        free(buf);
+        exit(EXIT_SUCCESS);
+    } result = -1;
     return (result);
 }
